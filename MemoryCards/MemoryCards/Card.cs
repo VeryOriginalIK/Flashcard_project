d1eeef8b2c6bb0b2fadb9f0df3bf4af236416c98
Vörös Bence <100135396+VeryOriginalIK@users.noreply.github.com>
2025-04-13 21:57:48 +0200

    init_ commit

---
+++ MemoryCards/MemoryCards/Card.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryCards
+{
+    class Card
+    {
+        public int id { get; private set; }
+        public string content { get; private set; }
+        public DateTime lastRead { get; set; }
+        public int timesRead { get; set; }
+        public string user { get; private set; }
+        public string answer { get; private set; }
+
+
+        public Card(int id, string content, DateTime lastRead, string user, string answer)
+        {
+            this.id = id;
+            this.content = content;
+            this.lastRead = lastRead;
+            this.timesRead = 0;
+            this.user = user;
+            this.answer = answer;
+        }
+    }
+
+}
